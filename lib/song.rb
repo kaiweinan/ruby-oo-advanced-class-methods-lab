833666f23799dc43a6fde9d2cfcdd7710e2f7fae
kaiweinan <kaiweinan@yahoo.com>
2021-01-09 16:00:30 -0500

    commit tree 1e3ca0eba86cfa7b8e09748c13a488935a304ee2

--- lib/song.rb
+++ lib/song.rb
@@ -10,4 +10,51 @@ class Song
     self.class.all << self
   end
 
+  def self.create
+  song=self.new
+  song.save
+  song
+  end
+  def Song.new_by_name(name)
+    song=self.new
+    song.name=name
+    song
+  
+  end
+def Song.create_by_name(name)
+  song=self.new
+  song.name=name
+  song.save
+  song
+end
+def Song.find_by_name(name)
+  @@all.detect {|song| song.name == name}
+end
+def Song.find_or_create_by_name(name)
+Song.find_by_name(name) || Song.create_by_name(name)
+    
+end
+def self.alphabetical
+@@all.sort {|a,b| a.name <=> b.name}
+end
+
+def self.new_from_filename(filename)
+split_filename = filename.chomp(".mp3").split(" - ")
+song = Song.new
+song.name = split_filename[1]
+song.artist_name = split_filename[0]
+song
+end
+
+def self.create_from_filename(filename)
+  split_filename = filename.chomp(".mp3").split(" - ")
+  song = Song.new
+  song.name = split_filename[1]
+  song.artist_name = split_filename[0]
+  song.save
+  song
+  end
+def self.destroy_all
+  @@all=[]
+end
 end
